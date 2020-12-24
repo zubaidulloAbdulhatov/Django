6e98629a2815f598e1105dcbdfa0ee9e995f0905
namat <namatullahwahidi@gmail.com>
2020-12-24 14:56:15 +0600

    some commits

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/muslim/PycharmProjects/shop/venv"
+set -gx VIRTUAL_ENV "/home/namat/Project/Django/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
